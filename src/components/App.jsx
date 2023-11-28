f16ffe5061f139b0b8b3f40f83625048c3141bfa
bahertylop <levichankin@gmail.com>
2023-11-28 14:28:28 +0300

    AppRoutes commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import AppRoutes from "./Routes/Routes.jsx";
+
+const App = () => {
+    return <div className="app">
+        <AppRoutes />
+    </div>;
+};
+
+export default App;
