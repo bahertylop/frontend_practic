8f3ddd9a467ae21dddfb6e7a194f5c2f73254e8a
bahertylop <levichankin@gmail.com>
2023-11-28 14:49:59 +0300

    arch commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import AppRoutes from "./Routes/Routes.jsx";
+import Header from "./Header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+import Sidebar from "./Sidebar/Sidebar.jsx";
 
 const App = () => {
-    return <div className="app">
-        <AppRoutes />
-    </div>;
+    return (
+        <div className="app">
+            <Header />
+            
+        <div className="container">
+            <Sidebar />
+            <AppRoutes />
+        </div>
+        
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
