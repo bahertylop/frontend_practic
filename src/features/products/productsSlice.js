c59d90825f3fe950ec8684f187263ed9575f4c1a
bahertylop <levichankin@gmail.com>
2023-11-29 21:22:14 +0300

    sneak.json commit

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -2,19 +2,34 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import { BASE_URL } from "../../utils/constants";
 import axios from 'axios'
 
+import data from "./products.json"
 
+
+// export const getProducts = createAsyncThunk(
+//     'products/getProducts', 
+//     async(_, thunkAPI) => {
+//         try {
+//             const res = await axios(`${BASE_URL}/products`);
+//             return res.data;
+//         } catch(err) {
+//             console.log(err);
+//             return thunkAPI.rejectWithValue(err);
+//         }
+//     }
+// );
 export const getProducts = createAsyncThunk(
     'products/getProducts', 
     async(_, thunkAPI) => {
         try {
-            const res = await axios(`${BASE_URL}/products`);
-            return res.data;
+            //const res = await axios(`${BASE_URL}/products`);
+            return data;
         } catch(err) {
             console.log(err);
             return thunkAPI.rejectWithValue(err);
         }
     }
 );
+
 
 const productsSlice = createSlice({
     name: "products",
