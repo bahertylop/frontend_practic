94c1e163e68e392dc953be8b7b5dfa64534d9859
bahertylop <levichankin@gmail.com>
2023-12-10 22:07:33 +0300

    categories done commit

--- src/components/Categories/Category.jsx
+++ src/components/Categories/Category.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import { useSelector } from "react-redux";
 import { filterById } from '../../features/products/productsSlice';
 import styles from "../../styles/Category.module.css";
 import Products from '../Products/Products';
@@ -7,69 +8,90 @@ import Products from '../Products/Products';
 const Category = () => {
 
     const { id } = useParams();
+    const { list } = useSelector(({ categories }) => categories);
 
+    const defaultValues = {
+        title: "",
+        price_min: 0,
+        price_max: 0,
+      };
+    
     const defaultParams = {
         title: "",
         price_min: 0,
         price_max: 0,
         categoryId: id
-    }
+      };
 
-    const [ params, setParams] = useState(defaultParams)
+    const [categor, setCategor] = useState("");
+    const [values, setValues] = useState(defaultValues);
+    const [params, setParams] = useState(defaultParams);
+    const products = filterById(id);
 
     useEffect(() => {
         if (!id) return;
 
-        setParams({ ...defaultParams, categoryId: id})
-    }, [id])
+        setParams({ ...defaultParams, categoryId: id });
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [id]);
 
-    const products = filterById({ id });
+    useEffect(() => {
+        if (!id || !list.length) return;
+
+        const { category } = list.find(( item ) => item.id.toString() === id)
+
+        setCategor(products[0].category.name)
+    }, [list, id])
+
+    console.log(list);
+    console.log(categor);
+
+    // const handleChange = ({ target: { value, name } }) => {
+    //     setValues({ ...values, [name]: value });
+    // };
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+
+    //     setParams({ ...params, ...values });
+    // };
     
     console.log(products);
   return (
     <section className={styles.wrapper}>
-        <h2 className={styles.title}>Category</h2>
+        <h2 className={styles.title}>{categor} </h2>
 
-        <form className={styles.filters} onSubmit={() => {}}>
+        {/* <form className={styles.filters} onSubmit={handleSubmit}>
             <div className={styles.filter}>
                 <input 
                     type="text" 
                     name="title" 
-                    onChange={()=>{}} 
+                    onChange={handleChange} 
                     placeholder="Product name" 
-                    value={params.title}
+                    value={values.title}
                 />
             </div>   
             <div className={styles.filter}>
                 <input 
                     type="number" 
                     name="price_min" 
-                    onChange={()=>{}} 
+                    onChange={handleChange} 
                     placeholder="0" 
-                    value={params.price_min}
+                    value={values.price_min}
                 />
             </div> 
             <div className={styles.filter}>
                 <input 
                     type="number" 
                     name="price_max" 
-                    onChange={()=>{}} 
+                    onChange={handleChange} 
                     placeholder="0" 
-                    value={params.price_max}
+                    value={values.price_max}
                 />
             </div>  
 
             <button type="submit" hidden></button>
-        </form>
-
-        {/* {isLoading ? (
-            <div className="preloader">Loading...</div>
-        ): !isSuccess || !products.length ? (
-            <div className={styles.back}>
-                <span>No results</span>
-                <button>Reset</button>
-            </div>
-        )} */}
+        </form> */}
 
         <Products title="" products={products} style={{ padding: 0}} amount={products.length} />
     </section>
--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -46,10 +46,49 @@ export function searchProducts(searchText) {
     return results;
   }
 
-export function filterById(targetId) {
-    return products.filter(item => item.category.id.toString() === targetId.id);
-};
+  export function filterById(targetId) {
+    console.log(targetId);
+    console.log(products[0].category.id);
+    return products.filter(item => item.category.id.toString() === targetId);
+}
 
+export function getFilteredProduct(values, categoryId) {
+    if (!values.price_min) {
+        values.price_min = 0;
+    }
+    if (!values.price_max) {
+        values.price_max = 1000;
+    }
+
+    if (!values.title) {
+        values.title = " ";
+    }
+
+    return products.filter(product => {
+        const categoryIdMatch = categoryId ? product.category.id.toString() === categoryId : true;
+        const titleMatch = product.title.toLowerCase().includes(values.title.toLowerCase());
+        const priceMatch = product.price >= values.price_min && product.price <= values.price_max;
+
+        return categoryIdMatch && titleMatch && priceMatch;
+    });
+}
+// export const selectProductsByCategoryId = (state, categoryId) => {
+//     return state.products.list.filter(product => product.category.id.toString() === categoryId);
+//   };
+
+// productsSlice.js
+export const selectProductsByCategoryId = (state, categoryId, title, price_min, price_max) => {
+    //const { title, price_min, price_max } = state.products.filters; // Предполагается, что filters - это объект с вашими параметрами
+  
+    return state.products.list.filter(product => {
+      const categoryIdMatch = product.category.id.toString() === categoryId;
+      const titleMatch = product.title.toLowerCase().includes(title.toLowerCase());
+      const priceMatch = product.price >= price_min && product.price <= price_max;
+  
+      return categoryIdMatch && titleMatch && priceMatch;
+    });
+  };
+  
 
 const productsSlice = createSlice({
     name: "products",
