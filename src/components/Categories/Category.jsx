7b0e71899f86b41e8cc62a4d265b6816369b30d2
bahertylop <levichankin@gmail.com>
2024-02-20 12:09:02 +0300

    вывод продуктов в категории

--- src/components/Categories/Category.jsx
+++ src/components/Categories/Category.jsx
@@ -1,99 +1,41 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useSelector } from "react-redux";
-import { filterById } from '../../features/products/productsSlice';
-import styles from "../../styles/Category.module.css";
+import axios from 'axios';
 import Products from '../Products/Products';
+import styles from "../../styles/Category.module.css";
+
+
+// добавить имя категории
 
 const Category = () => {
-
     const { id } = useParams();
-    const { list } = useSelector(({ categories }) => categories);
-
-    const defaultValues = {
-        title: "",
-        price_min: 0,
-        price_max: 0,
-      };
-    
-    const defaultParams = {
-        title: "",
-        price_min: 0,
-        price_max: 0,
-        categoryId: id
-      };
-
-    const [categor, setCategor] = useState("");
-    const [values, setValues] = useState(defaultValues);
-    const [params, setParams] = useState(defaultParams);
-    const products = filterById(id);
+    const [products, setProducts] = useState([]);
+    // const [categoryName, setCategoryName] = useState('');
 
     useEffect(() => {
-        if (!id) return;
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8080/api/categories/${id}`);
+                const data = response.data;
+                setProducts(data);
+                // if (data.length > 0) {
+                //     // Устанавливаем имя категории из первого продукта в списке
+                //     setCategoryName(data[0].category.name);
+                // }
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
 
-        setParams({ ...defaultParams, categoryId: id });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
+        fetchProducts();
     }, [id]);
 
-    useEffect(() => {
-        
-
-        setCategor(products[0].category.name)
-    }, [list, id])
-
-    console.log(list);
-    console.log(categor);
-
-    // const handleChange = ({ target: { value, name } }) => {
-    //     setValues({ ...values, [name]: value });
-    // };
-
-    // const handleSubmit = (e) => {
-    //     e.preventDefault();
-
-    //     setParams({ ...params, ...values });
-    // };
-    
-    console.log(products);
-  return (
-    <section className={styles.wrapper}>
-        <h2 className={styles.title}>{categor} </h2>
-
-        {/* <form className={styles.filters} onSubmit={handleSubmit}>
-            <div className={styles.filter}>
-                <input 
-                    type="text" 
-                    name="title" 
-                    onChange={handleChange} 
-                    placeholder="Product name" 
-                    value={values.title}
-                />
-            </div>   
-            <div className={styles.filter}>
-                <input 
-                    type="number" 
-                    name="price_min" 
-                    onChange={handleChange} 
-                    placeholder="0" 
-                    value={values.price_min}
-                />
-            </div> 
-            <div className={styles.filter}>
-                <input 
-                    type="number" 
-                    name="price_max" 
-                    onChange={handleChange} 
-                    placeholder="0" 
-                    value={values.price_max}
-                />
-            </div>  
-
-            <button type="submit" hidden></button>
-        </form> */}
-
-        <Products title="" products={products} style={{ padding: 0}} amount={products.length} />
-    </section>
-  )
-}
+    return (
+        <section className={styles.wrapper}>
+            <h2 className={styles.title}>Shoes</h2>
+            <Products products={products} style={{ padding: 0 }} amount={products.length} />
+        </section>
+    );
+};
 
 export default Category;
