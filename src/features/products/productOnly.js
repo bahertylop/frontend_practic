78a9588a798cdf8df699563876f7721b560e41ed
bahertylop <levichankin@gmail.com>
2024-03-11 18:58:57 +0300

    починил

--- src/features/products/productOnly.js
+++ src/features/products/productOnly.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import axios from 'axios'
 
-import data from "./products.json"
+// import data from "./products.json"
 import {ROUTES} from "../../utils/routes";
 
 
